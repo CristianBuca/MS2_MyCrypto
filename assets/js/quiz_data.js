fc22a2275d1f1c593d04832162e8b46e1bcc573a
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2021-10-14 00:45:58 +0100

    Add(script): array with question data for quiz

---
+++ assets/js/quiz_data.js
@@ -0,0 +1,32 @@
+const questions = [
+  {
+    question: "Which mining algorithm does Monero use?",
+    choices: ["CryptoNight", "RandomX", "SHA-256"],
+    correct: "RandomX",
+    validation: "Monero upgraded to RandomX, a new mining algorithm that claims to be ASIC-resistant, at the start of Decemeber 2019.",
+  },
+  {
+    question: "Which tokens are traded on crypto exchanges?",
+    choices: ["ERC-20", "ERC-721", "Nonfungible tokens"],
+    correct: "ERC-20",
+    validation: "These tokens, designed for use on the Etherium platform, are available on a plethora of exchanges.",
+  },
+  {
+    question: "How is the Unspent Transaction Output, or UTXO, best described?",
+    choices: ["It's like having a bank account", "It's like paying with cash", "The main model used by Ethereum"],
+    correct: "It's like paying with cash",
+    validation: "This special transaction resends unspent coins from every transaction - kind of like receiving change after paying with a banknote.",
+  },
+  {
+    question: "Which is most likely to result in crypto being sent to the wrong address?",
+    choices: ["Copying and pasting the address", "Always check the first and last four letters of the crypto address, as well as a few in the middle",  "Test the address with a small transaction before making a substantial deposit"],
+    correct: "Copying and pasting the address",
+    validation: "This can be incredibly risky, as clipboard-kidnapping software could place another wallet address in place of the correct one.",
+  },
+  {
+    question: "Who is the proud CEO of Changelly?",
+    choices: ["Elon Musk", "Eric Benz", "Gavin Belson", "Dr. Ruja Ignatova"],
+    correct: "Eric Benz",
+    validation: "A crypto expert and a big believer in both mass adoption and Changelly's mission to make crypto as accessible as possible.",
+  },
+]
