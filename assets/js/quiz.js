a2bd41caba68d9d539a9004de066de3024994eab
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2021-10-14 02:20:16 +0100

    End(script): displayQuestion function

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -5,7 +5,7 @@ const quizStart = $('#quiz-start');
 const quizNext = $('#quiz-next');
 const quizResults = $('#quiz-results');
 const quizRestart = $('#quiz-restart');
-
+var quizIndex = 0;
 
 // Functions
 
@@ -13,8 +13,18 @@ const quizRestart = $('#quiz-restart');
 // for Start Game and Next Question buttons
 function displayQuestion() {
 // Clear previous data from quiz card
-  $('#quiz-question').text('');
   $('#quiz-answers').text('');
+// Add question text to the "quiz-question" div
+  $('#quiz-question').text(questions[quizIndex].question);
+// Take answers for current question from the questions.choices array and assign them to a new array
+  let answers = questions[quizIndex].choices;
+// Iterate the array, create dynamic buttons for each choice using template literals
+// and append them to the "quiz-answers" div
+  answers.forEach(choice => {
+    $(`<button class="answer active">${choice}</button>`).appendTo('#quiz-answers');
+  });
+// Increment quizIndex for next question
+  quizIndex ++;
 }
 
 // Event Listeners
