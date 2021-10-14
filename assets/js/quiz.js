ba386c3d070c385598de59d086678f3537ffa96e
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2021-10-14 02:42:10 +0100

    Reformat(script): switch to DocStrings comments

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -9,36 +9,38 @@ var quizIndex = 0;
 
 // Functions
 
-// Function to display next question will be used by event listeners 
-// for Start Game and Next Question buttons
+/**
+Function to display next question which will be used by event listeners for Start Game and Next Question buttons
+- Clear previous data from quiz card
+- Add question text to the "quiz-question" div
+- Take answers for current question from the questions.choices array and assign them to a new array
+- Iterate the array, create dynamic buttons for each choice using template literals and append them to the "quiz-answers" div
+- Increment quizIndex for next question
+ */
+
 function displayQuestion() {
-// Clear previous data from quiz card
   $('#quiz-answers').text('');
-// Add question text to the "quiz-question" div
   $('#quiz-question').text(questions[quizIndex].question);
-// Take answers for current question from the questions.choices array and assign them to a new array
   let answers = questions[quizIndex].choices;
-// Iterate the array, create dynamic buttons for each choice using template literals
-// and append them to the "quiz-answers" div
   answers.forEach(choice => {
     $(`<button class="answer active">${choice}</button>`).appendTo('#quiz-answers');
   });
-// Increment quizIndex for next question
   quizIndex ++;
 }
 
 // Event Listeners
 
 // Listens for the Start Game button click and displays first question
+// Hides the Start Game Button and shows the Next Question Button
 quizStart.on('click', function() {
   displayQuestion();
-// Hides the Start Game Button and shows the Next Question Button
   quizStart.addClass('hide');
   quizNext.removeClass('hide');
 });
 
 // Listens for the Next Question button click and based on the current questionIndex
-// displays the next question if available or reveals the show Show Results button to end game
+// Displays the next question if available or reveals the show Show Results button to end game
+
 quizNext.on('click', function() {
   if (quizIndex < questions.length) displayQuestion();
   else {
