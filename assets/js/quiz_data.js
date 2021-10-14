5a2d65d76e6f4b5b3bc8902e2fc9c3137997dff3
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2021-10-14 01:37:53 +0100

    End(script): Complete questions data array

--- assets/js/quiz_data.js
+++ assets/js/quiz_data.js
@@ -1,3 +1,7 @@
+
+// Each question object is structured with keys for question text, array with answer choices, 
+// correct choice and extra validation message for correct choice.
+
 const questions = [
   {
     question: "Which mining algorithm does Monero use?",
@@ -29,4 +33,40 @@ const questions = [
     correct: "Eric Benz",
     validation: "A crypto expert and a big believer in both mass adoption and Changelly's mission to make crypto as accessible as possible.",
   },
+  {
+    question: "What's the biggest advantage of using a centralized exchange?",
+    choices: ["Higher liquidity and a higher amount of trading pairs", "No trading limits", "Users get total ownership of their funds"],
+    correct: "Higher liquidity and a higher amount of trading pairs",
+    validation: "With centralized exchanges, it can be easier to swap between a greater number of fiat currencies and cryptos, and find an appropriate buyer or seller quickly.",
+  },
+  {
+    question: "What is the main benefit of dollar-pegged coins?",
+    choices: ["All of them are officially audited", "They avoid price fluctuations by using mechanisms so their value remains close to the U.S. dollar", "Major stablecoins such as Tether are easy to sell for USD"],
+    correct: "They avoid price fluctuations by using mechanisms so their value remains close to the U.S. dollar",
+    validation: "Dollar-pegged coins are especially useful for those who want to protect their capital from dramatic price swings in the crypto market.",
+  },
+  {
+    question: "Which Country is strictest about initial coin offerings?",
+    choices: ["United States", "Singapore", "Switzerland"],
+    correct: "United States",
+    validation: "Some critics argue that many ICOs count as securities, meaning they should fall under regulatory oversight. This has led many crypto startups in foreign countries to stop U.S. citizens from participating.",
+  },
+  {
+    question: "Which of these is not a risk associated with cryptocurrency trading?",
+    choices: ["Trading only takes place during U.S. business hours, so it's easy to miss out on price movements", "Funds in exchanges can be put at risk if the trading platform is shut down, hacked or bankrupted by its owners", "The industry is largely unregulated, so many of the policies offered by crypto exchanges are not enforceable"],
+    correct: "Trading only takes place during U.S. business hours, so it's easy to miss out on price movements",
+    validation: "This isn't actually true. A lack of centralized governance means it's possible to buy and sell cryptocurrencies 24/7",
+  },
+  {
+    question: "How can you prevent falling victim to a fraudulent exchange?",
+    choices: ["Rely on reviews of crypto exchanges from high-profile YouTubers", "Find crypto exchanges that offer free giveaways and high rates of returns", "Bookmark login pages and check the address bar before committing your password"],
+    correct: "Bookmark login pages and check the address bar before committing your password",
+    validation: "Although there are no guarantees that this is bulletproof, always double-checking a page thoroughly before you log in can prevent details being compromised.",
+  },
+  {
+    question: "Finish the sentence: When buying a crypto asset, you should…",
+    choices: ["Only invest as much as you can afford to lose", "Rely on data that shows price movements over the past 24 hours", "Follow what popular crypto analysts on Twitter are doing"],
+    correct: "Only invest as much as you can afford to lose",
+    validation: "Always remember that cryptocurrencies are extremely volatile — and there’s no predicting what will happen next. By investing conservatively, you can shield yourself against the risk of losing your hard-earned funds.",
+  }
 ]
