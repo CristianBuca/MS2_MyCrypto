667d094e850054e4016553cc6b98047eb69755b4
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2021-10-14 01:44:06 +0100

    Add(script): quiz control global variables

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,5 +1,10 @@
 // Global Variables
 
+// Quiz control buttons into global variables
+var quizStart = $('#quiz-start');
+var quizNext = $('#quiz-next');
+var quizResults = $('#quiz-results');
+var quizRestart = $('#quiz-restart');
 
 // Functions
 
