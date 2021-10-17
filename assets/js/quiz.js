dadd82557f0cc810f835d825215a1012ac26ae02
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2021-10-17 16:26:56 +0100

    Document(script): Add docString for switchTheme()

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -93,6 +93,13 @@ function showResults() {
   $('#quiz-restart').removeClass('hide');
 }
 
+/**
+ * Function to maintain theme selection during the session
+ * Uses session storage data from last #theme-switch change
+ * and sets the body and #theme-switch state to last known 
+ * selection after page reload.
+ */
+
 function switchTheme() {
   if (sessionStorage.getItem('theme')) {
     $('body').addClass('dark');
@@ -137,7 +144,7 @@ $('#quiz-info').on('click', () => $('#info-modal-container').css({'opacity' : '1
 $('#modal-close').on('click', () => $('#info-modal-container').css({'opacity' : '0', 'pointer-events' : 'none'}));
 
 // Listens for checkbox state change and toggles theme on body
-// Uses session storage to remember theme selection on reload
+// Uses session storage to store theme selection state
 $('#theme-switch').on('change', () => {
   $('body').toggleClass('dark');
   $('body').hasClass('dark') ? sessionStorage.setItem('theme', 'dark') : sessionStorage.removeItem('theme');
