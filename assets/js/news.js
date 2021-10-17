d00867853857ad8db6914e28a3c907dd935814a4
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2021-10-17 21:35:30 +0100

    Start(script): Add script for theme switch

---
+++ assets/js/news.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+// Functions
+
+/**
+ * Function to maintain theme selection during the session
+ * Uses session storage data from last #theme-switch change
+ * and sets the body and #theme-switch state to last known 
+ * selection after page reload.
+ */
+
+ function switchTheme() {
+  if (sessionStorage.getItem('theme')) {
+    $('body').addClass('dark');
+    $('#theme-switch input').prop('checked', true);
+  }
+}
+
+
+// Listeners
+
+// Listens for checkbox state change and toggles theme on body
+// Uses session storage to store theme selection state
+$('#theme-switch').on('change', () => {
+  $('body').toggleClass('dark');
+  $('body').hasClass('dark') ? sessionStorage.setItem('theme', 'dark') : sessionStorage.removeItem('theme');
+});
+
+switchTheme();
