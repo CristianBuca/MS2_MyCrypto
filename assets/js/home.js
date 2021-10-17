fea55695fc6eb24553eeb8f3f1e59e423db5d6d2
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2021-10-17 23:13:10 +0100

    Script(index): async fetch coins from coingeko

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,9 +1,16 @@
-
-
-
-
+// Global variables
+var data = []
 
 // Functions
+
+async function getCoins() {
+  try {
+      const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+      data = await res.json();
+  } catch (error) {
+      console.log('Error:' + error);
+  }
+}
 
 /**
  * Function to maintain theme selection during the session
@@ -30,3 +37,5 @@ $('#theme-switch').on('change', () => {
 });
 
 switchTheme();
+
+getCoins();
