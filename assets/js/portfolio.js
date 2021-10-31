d65d54e2420eadf4c26546db84d17839b8226bcf
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2021-10-31 21:24:32 +0000

    Script(portfolio): Document comments and credits

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -5,6 +5,10 @@ var portfolio = new Array();
 
 // Functions
 
+/**
+ * Async function to fetch all coins and convert response in JSON format
+ * Uses .map method to generate the options in the searchable dropdown input list.
+ */
 async function getCoinList() {
   try {
     const response = await fetch("https://api.coingecko.com/api/v3/coins/list");
@@ -16,7 +20,7 @@ async function getCoinList() {
 }
 
 /**
- * This function checks if the user has input the name or the cryptocurrency symbol
+ * Checks if the user has input the name or the cryptocurrency symbol
  * against the list of coins retrieved from the API.
  * For a user-friendly experience all strings are converted to lowercase before being checked.
  * Function uses Local Storage to keep track of the user's portfolio between sessions.
@@ -25,6 +29,7 @@ async function getCoinList() {
   const key = $('#coin-select')[0].value;
   const amount = $('#inp-value')[0].value;
 
+// ForEach loop to check for a valid entry before adding the asset to localStorage.
   coinList.forEach(item => {
     if ((key.toLowerCase() === item.symbol.toLowerCase() || key.toLowerCase() === item.name.toLowerCase()) && amount) 
     localStorage.setItem(item.id, amount);
@@ -33,7 +38,7 @@ async function getCoinList() {
 }
 
 /**
- * This function selects the hidden currency ID stored in the asset row that is selected for removal
+ * Selects the hidden currency ID stored in the asset row that is selected for removal
  * by the event listener and removes it from localStorage, then removes the assets table row.
  * Credit to Codepedia for the syntax: https://codepedia.info/jquery-get-table-cell-td-value-div/
  */
@@ -43,17 +48,30 @@ function removeAsset() {
   $(this).closest('tr').remove();
 }
 
-// Credit for iterating localStorage https://stackoverflow.com/a/37507935
+
+/** 
+ * Generates a portfolio array from localStorage data.
+ * Credit for iterating localStorage https://stackoverflow.com/a/37507935
+ */
 function buildPortfolio() {
+// ForEach loop to iterate through localStorage by it's keys and push entries in array.
   Object.keys(localStorage).forEach(asset => {
     portfolio.push({coin: asset, amount: localStorage.getItem(asset)});
   });
 }
 
+/**
+ * Async function to fetch information only for the coins added to the portfolio.
+ * Converts response to JSON.
+ * Uses @method find to match entries from response with the ones in the portfolio by ID.
+ * Calculates asset worth and total portfolio worth
+ * Display a dynamic table with fetched data based on portfolio.
+ */
 async function displayPortfolio() {
   const api = 'https://api.coingecko.com/api/v3/coins/markets'
   try {
-    // Get ids to be used in api url from portfolio into a string separated by ","
+
+// Get ids to be used in api url from portfolio into a string separated by ","
     let ids = portfolio.map(asset => asset.coin.toLowerCase()).join(',');
     let url = `${api}?vs_currency=usd&ids=${ids}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
     const response = await fetch(url);
@@ -71,7 +89,7 @@ async function displayPortfolio() {
           ])
         ])
       ])
-
+// ForEach loops through the received data array.
     data.forEach(asset => {
       var match = portfolio.find(obj => {
         return obj.coin === asset.id;
@@ -94,14 +112,14 @@ async function displayPortfolio() {
   }
   catch(e) {
     console.error(e);
-    // alert('CoinGeko is a bit slow sending the data. Try again later');
   }
 }
 
 /**
- * Function to maintain theme selection through the session.
- * Uses session storage data from last #theme-switch change and sets the body and #theme-switch 
- * state to last known selection after page reload.
+ * Function to maintain theme selection during the session.
+ * Uses session storage data from last #theme-switch change
+ * and sets the body and #theme-switch state to last known 
+ * selection after page reload.
  */
  function switchTheme() {
   if (sessionStorage.getItem('theme') === 'dark') {
@@ -112,10 +130,11 @@ async function displayPortfolio() {
 
 // Event Listeners
 
-// Listens for checkbox state change and toggles theme on body
-// Uses session storage to store theme selection state
+// Listens for checkbox state change and toggles theme on body.
+// Uses session storage to store theme selection state.
 $('#theme-switch').on('change', () => {
   $('body').toggleClass('dark light');
+// Conditional statement written in shorthand. Checks current theme state and changes the value in session storage.
   $('body').hasClass('dark') ? sessionStorage.setItem('theme', 'dark') : sessionStorage.setItem('theme', 'light');
 });
 
