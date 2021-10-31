343f46b5042f16a756a95db6906e7b0b76e48adc
CristianBuca <79543676+CristianBuca@users.noreply.github.com>
2021-10-31 14:17:01 +0000

    Script(email): Add validateForm function

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,18 +1,36 @@
-// Credit to Code Institute lesson "Sending Emails Using EmailJS"
+/* jshint esversion: 8 */
 
-function sendMail(contactForm) {
+// Credit for email validation expression: https://stackoverflow.com/a/9204568
+function validateEmail(email) {
+	var em = /\S+@\S+\.\S+/;
+	return em.test(email);
+}
+
+function validateForm() {
+	if ($('#userName').val() === '') popUp($('#userName'));
+		else if (!validateEmail($('#email').val())) console.log('error e');
+			else if ($('#message').val() === '') console.log('error m');
+				else sendMail();
+	return false;
+}
+
+// Credit to Code Institute lesson "Sending Emails Using EmailJS"
+// Credit to EmailJS API documentation: https://www.emailjs.com/docs/ 
+function sendMail() {
   emailjs.init("user_iyGKuAhxS6sgm3Z6ZvXd0");
   emailjs.send("service_03dgxm2", "mycrypto", {
-      "from_name": contactForm.userName.value,
-      "from_email": contactForm.email.value,
-      "message": contactForm.message.value,
+      "from_name": $('#userName').val(),
+      "from_email": $('#email').val(),
+      "message": $('#message').val(),
   })
   .then(
       function(response) {
-          console.log("Message Sent", response);
+          alert('Message Sent');
+          console.log('Message Sent', response);
       },
       function(error) {
-          console.log("Failed", error);
+          alert('Unable to Send Message :( Please try again later.')
+          console.log('Failed', error);
       }
   );
   return false; //to prevent the page from refreshing when submitting the form
